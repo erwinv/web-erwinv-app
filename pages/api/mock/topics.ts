2a0e2ab69c4c14be8d16ec389eb511ddfd73c503
Erwin Villejo <erwin.villejo@gmail.com>
2021-05-07 23:36:23 +0700

    make ssr data dynamic instead of static

--- pages/api/mock/topics.ts
+++ pages/api/mock/topics.ts
@@ -1,17 +1,21 @@
 import _ from 'lodash'
 import faker from 'faker'
 import { NextApiHandler } from 'next'
-import { Topic } from '../../../types'
+import { Article, Topic } from '../../../types'
 import { articles } from './articles'
 
-export const topics = _.chain(articles)
-  .map(a => _.pick(a, 'id', 'title'))
-  .chunk(_.floor(articles.length / 10))
-  .map(articles => ({
-    name: faker.lorem.words(_.random(2, 4)),
-    articles,
-  }))
-  .value()
+export function fakeTopics(articles: Article[]) {
+  return _.chain(articles)
+    .map(a => _.pick(a, 'id', 'title'))
+    .chunk(_.floor(articles.length / 10))
+    .map(articles => ({
+      name: faker.lorem.words(_.random(2, 4)),
+      articles,
+    }))
+    .value()
+}
+
+export const topics = fakeTopics(articles)
 
 const getTopics: NextApiHandler<Topic[]> = (req, res) => {
   if (req.method !== 'GET') {
--- pages/ssr/docs.ts
+++ pages/ssr/docs.ts
@@ -1,13 +1,18 @@
+import _ from 'lodash'
 import { GetServerSideProps } from 'next'
 import Docs, { DocsProps } from '../docs'
-import { topics } from '../api/mock/topics'
+import { fakeArticle } from '../api/mock/articles'
+import { fakeTopics } from '../api/mock/topics'
 
 export default Docs
 
 export const getServerSideProps: GetServerSideProps<DocsProps> = async (context) => {
+  const articles = _.range(100).map(fakeArticle)
+  const topics = fakeTopics(articles)
+  
   return {
     props: {
-      topics
+      topics,
     }
   }
 }
